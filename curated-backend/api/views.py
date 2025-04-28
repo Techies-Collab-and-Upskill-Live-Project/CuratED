28ec3d102ec581235347acbf05433f00eaf1f9f2
mayowa-kalejaiye <kalejaiyemayowa3@gmail.com>
2025-04-28 19:35:43 +0100

    Initial commit

--- curated-backend/api/views.py
+++ curated-backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .youtube import fetch_videos_by_keyword
 
-# Create your views here.
+
+@api_view(['GET'])
+def search_videos(request):
+    quesry = request.GET.get('q')
+    if not query:
+        return Response
---
+++ curated-backend/api/youtube.py
@@ -0,0 +1,19 @@
+import requests
+from decouple import config
+
+
+API_KEY = config("YOUTUBE_API_KEY")
+SEARCH_URL = "https://www.googleapis.com/youtube.v3/search"
+
+def fetch_videos_by_keyword(query, max_results=10):
+    params = {
+        'part': 'snippet',
+        'q': query,
+        'key': API_KEY,
+        'maxResults': max_results,
+        'type': 'video',
+    }
+    response = requests.get(SEARCH_URL, params=params)
+    if response.status_code == 200:
+        return response.json()
+    return  {"error":"Failed to fetch from Youtube"}
