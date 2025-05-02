9840ab47a27448fd387df6f2c5c1ae44adac9390
mayowa-kalejaiye <kalejaiyemayowa3@gmail.com>
2025-05-02 22:38:35 +0100

    feat: Implement YouTube search API with endpoint and response handling

---
+++ curated-backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import YouTubeSearchAPIView
+
+urlpatterns = [
+    path('api/search', YouTubeSearchAPIView.as_view(), name='youtube_search'),
+]
+
--- curated-backend/api/views.py
+++ curated-backend/api/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .youtube import fetch_videos_by_keyword
+from rest_framework import status
+from .youtube import fetch_videos_by_keyword 
 
+class YouTubeSearchAPIView(APIView):
+    def get(self, request):
+        query = request.query_params.get('q')
+        if not query:
+            return Response({"error": "Search query is required."}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def search_videos(request):
-    quesry = request.GET.get('q')
-    if not query:
-        return Response
+        results = fetch_videos_by_keyword(query)
+        if "error" in results:
+            return Response({"error": "Failed to fetch YouTube."}, status=status.HTTP_502_BAD_GATEWAY)
+        
+        videos = []
+        for item in results.get("items", []):
+            videos.append({
+                "videoId": item["id"]["videoId"],
+                "title": item["snippet"]["title"],
+                "description": item["snippet"]["description"],
+                "thumbnail": item["snippet"]["thumbnails"]["high"]["url"],
+                "channel": item["snippet"]["channelTitle"],
+            })
+        return Response({"results": videos}, status=status.HTTP_200_OK)
