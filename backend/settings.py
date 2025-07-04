3966a8cfb29a9e1ce4582b58fd93e1b63a3e6648
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2025-07-04 18:06:31 +0100

    Add: fix the corsheader error

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
-CORS_ALLOWED_ORIGINS = [origin.strip() for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")]
+CORS_ALLOWED_ORIGINS = 'devcurated.vercel.app', 'localhost:3000'
 YOUTUBE_API_KEY = config('YOUTUBE_API_KEY')
 
 # Application definition
